8ac1d0628a23e3a4471dcf7327f70b72d04c4323
Abraham <abrahamlega@gmail.com>
2024-01-15 08:34:02 +0000

    Fixed env variables

--- src/helpers/getEnvironments.ts
+++ src/helpers/getEnvironments.ts
@@ -3,11 +3,12 @@ export const getEnvironments = () => {
 	return {
 		//...import.meta.env,
 		VITE_APIKEY: import.meta.env.VITE_APIKEY,
-		VITE_AUTHDOMAIN: import.meta.env.VITE_MODE,
+		VITE_AUTHDOMAIN: import.meta.env.VITE_AUTHDOMAIN,
 		VITE_PROJECTID: import.meta.env.VITE_PROJECTID,
 		VITE_STORAGEBUCKET: import.meta.env.VITE_STORAGEBUCKET,
 		VITE_MESSAGINGSENDERID: import.meta.env.VITE_MESSAGINGSENDERID,
 		VITE_APPID: import.meta.env.VITE_APPID,
 		VITE_API_URL: import.meta.env.VITE_API_URL,
+		VITE_MODE: import.meta.env.VITE_MODE
 	};
 };
